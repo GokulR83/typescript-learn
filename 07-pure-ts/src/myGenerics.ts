d0dc776daff57ba5403e186bcaa113aecec19924
GokulR83 <rajugokul003@gmail.com>
2025-02-02 17:53:36 +0530

    commit check

--- 07-pure-ts/src/myGenerics.ts
+++ 07-pure-ts/src/myGenerics.ts
@@ -1,119 +1,118 @@
 //? Generics in Typescript
 console.log("generic file");
 
+//general log
+
 const score: Array<number> = [];
 
 const names: Array<string> = [];
 
-function identityOne(num: number | boolean ): boolean | number {
-    return num;
+function identityOne(num: number | boolean): boolean | number {
+  return num;
 }
 
 //& in this function we can pass any type and return any type (like passing number and returning string)
-function identityTwo(val: any): any{
-    return val;
+function identityTwo(val: any): any {
+  return val;
 }
 
 //? here in this function, whatever you pass as type , it ensure the same return type
-function identityThree<Type>(val: Type):Type{
-    if(typeof val === 'string'){
-        return val;
-    }
-    else
+function identityThree<Type>(val: Type): Type {
+  if (typeof val === "string") {
     return val;
+  } else return val;
 }
 identityThree(3); //$ it only return the value that you're passing
 
 //* Another way to declare generics
-function identityFour<T>(val: T): T{
-    return val;
+function identityFour<T>(val: T): T {
+  return val;
 }
 
-
-interface Cricket{
-    format: string,
-    overs: number,
-    time: number
+interface Cricket {
+  format: string;
+  overs: number;
+  time: number;
 }
 
-function identityFive<Cricket>(val: Cricket): Cricket{
-    val = {
-        ...val,
-        time: 200
-    }
-    console.log(val);
-    return val; 
+function identityFive<Cricket>(val: Cricket): Cricket {
+  val = {
+    ...val,
+    time: 200,
+  };
+  console.log(val);
+  return val;
 }
 
 identityFive({
-    format:"t20i",
-    overs:20,
-    time: 180
+  format: "t20i",
+  overs: 20,
+  time: 180,
 });
 
 //* Generic with Array
-function getSearchProduct<Type>(products: Type[]): Type[]{
-    return products;
+function getSearchProduct<Type>(products: Type[]): Type[] {
+  return products;
 }
 
 //* Generic with arrow function
-const getMoreSearchProduct = <T>(products: Array<T>) : T =>{
-    return products[3];
-}
+const getMoreSearchProduct = <T>(products: Array<T>): T => {
+  return products[3];
+};
 
 //* Normal Arrow function with type
 
-const getDetails = ( num: number ):number =>{
-    return num;
+const getDetails = (num: number): number => {
+  return num;
+};
+
+function newFunction<T, U extends string>(valOne: T, valTwo: U): object {
+  return {
+    valOne,
+    valTwo,
+  };
 }
 
+newFunction(2, "3");
 
-function newFunction<T,U extends string>(valOne: T, valTwo: U):object{
-    return {
-        valOne,
-        valTwo
-    }
+interface BlogDatabase {
+  _id: string;
+  name: string;
+  summary: string;
+  comments: Array<string>;
 }
 
-newFunction(2,"3");
-
-
-interface BlogDatabase{
-    _id: string,
-    name: string,
-    summary: string,
-    comments: Array<string>
-}
-
-function fetchOneFromBlogDatabase<BlogDatabase,key extends keyof BlogDatabase>( blog: BlogDatabase, name:key ) {
-    return blog[name];
+function fetchOneFromBlogDatabase<BlogDatabase, key extends keyof BlogDatabase>(
+  blog: BlogDatabase,
+  name: key
+) {
+  return blog[name];
 }
 
 //& Generic class
-interface Course{
-    name: string,
-    department: "IT" | "CSE" | "AIML",
-    students: number
-    rank: string
+interface Course {
+  name: string;
+  department: "IT" | "CSE" | "AIML";
+  students: number;
+  rank: string;
 }
 
-interface Quiz{
-    name: string,
-    course: Course,
-    participants: number,
-    mark: number
+interface Quiz {
+  name: string;
+  course: Course;
+  participants: number;
+  mark: number;
 }
 
 class Conduct<T> {
-    public duration: number = 120
-    public questions: T[] = []
-    
-    start(quiz: T){
-        this.duration = 90;
-    }
+  public duration: number = 120;
+  public questions: T[] = [];
 
-    end(quiz: T){
-        this.duration = 0;
-    }
+  start(quiz: T) {
+    this.duration = 90;
+  }
 
+  end(quiz: T) {
+    this.duration = 0;
+  }
 }
